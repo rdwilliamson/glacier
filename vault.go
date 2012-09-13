ba25b390ce49cf5fef627abe0abf4d2e195e6860
Robert Williamson <williamson.robert@gmail.com>
2012-09-12 20:45:08 -0500

    Added calls to glacier functions.

--- vault.go
+++ vault.go
@@ -16,6 +16,25 @@ import (
 
 func vault() {
 	switch flag.Arg(2) {
+	case "create":
+		err := connection.CreateVault(flag.Arg(3))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	case "delete":
+		err := connection.DeleteVault(flag.Arg(3))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	case "describe":
+		vault, err := connection.DescribeVault(flag.Arg(3))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Printf("%+v\n", vault)
 	case "list":
 		_, vaults, err := connection.ListVaults(0, "")
 		if err != nil {
@@ -23,7 +42,21 @@ func vault() {
 			os.Exit(1)
 		}
 		fmt.Printf("%+v\n", vaults)
+	case "notification":
+		if flag.NArg() < 5 {
+			fmt.Println("no notification command")
+			os.Exit(1)
+		}
+		switch flag.Arg(3) {
+		case "set":
+		case "get":
+		case "delete":
+		default:
+			fmt.Println("unknown notification command:", flag.Arg(3))
+			os.Exit(1)
+		}
 	default:
 		fmt.Println("unknown vault command:", flag.Arg(2))
+		os.Exit(1)
 	}
 }
