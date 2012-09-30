09b756f12277e9346bc39672d26b832f69ce9139
Robert Williamson <williamson.robert@gmail.com>
2012-09-30 11:47:53 -0500

    Added archive jobs.

--- job.go
+++ job.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 // $ glacier us-east-1 job inventory <vault> <description> <topic>
+// $ glacier us-east-1 job archive <vault> <archive> <description> <topic>
 // $ glacier us-east-1 job list <vault>
 // $ glacier us-east-1 job get inventory <vault> <job>
+// $ glacier us-east-1 job get archive <vault> <job> <file>
 
 func job(args []string) {
 	if len(args) < 1 {
@@ -40,9 +43,32 @@ func job(args []string) {
 			os.Exit(1)
 		}
 		fmt.Println(jobId)
+
 	case "archive":
-		fmt.Println("not implemented")
-		os.Exit(1)
+		if len(args) < 2 {
+			fmt.Println("no vault")
+			os.Exit(1)
+		}
+		vault := args[0]
+		archive := args[1]
+		args = args[2:]
+
+		var description, topic string
+		if len(args) > 0 {
+			description = args[0]
+		}
+		if len(args) > 1 {
+			topic = args[1]
+		}
+
+		jobId, err := connection.InitiateRetrievalJob(vault, archive,
+			topic, description)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(jobId)
+
 	case "list":
 		if len(args) < 1 {
 			fmt.Println("no vault")
@@ -81,9 +107,35 @@ func job(args []string) {
 			}
 
 			fmt.Printf("%+v\n", inventory)
+
 		case "archive":
-			fmt.Println("not implemented")
-			os.Exit(1)
+			if len(args) < 3 {
+				fmt.Println("no vault, job id, and/or output file")
+				os.Exit(1)
+			}
+			vault := args[0]
+			job := args[1]
+			fileName := args[2]
+
+			file, err := os.Create(fileName)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			defer file.Close()
+
+			archive, err := connection.GetRetrievalJob(vault, job, 0, 0)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			defer archive.Close()
+
+			_, err = io.Copy(file, archive)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
 		default:
 			fmt.Println("unknown job sub command:", subCommand)
 			os.Exit(1)
